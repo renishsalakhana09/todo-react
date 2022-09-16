dbd4432f98890e57da50f8584d145315d852a3b8
Renish Salakhana <renishsalakhana@gmail.com>
2022-09-16 14:34:59 +0530

    Updates

---
+++ precache-manifest.e474d5eeb01b85f9537ed48d29f4bc51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d23c8d66870543ba18a2f5936fbbfd",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "4f30576e57848b6183e1",
+    "url": "/todo-react/static/css/main.7f5911e0.chunk.css"
+  },
+  {
+    "revision": "49cdc6123ed38c98312a",
+    "url": "/todo-react/static/js/2.e28fddec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.e28fddec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f30576e57848b6183e1",
+    "url": "/todo-react/static/js/main.696716df.chunk.js"
+  },
+  {
+    "revision": "d883ecf91dbef978f979",
+    "url": "/todo-react/static/js/runtime-main.e133de08.js"
+  }
+]);
